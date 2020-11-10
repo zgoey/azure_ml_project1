8a9d27fa7d8fd6132d9c8f3a05b0bbc319985615
Zoé Goey <zoe.goey@gmail.com>
2020-11-10 01:06:22 +0100

    Training script  and code for hyper-parameter tuning for first Azure ML project (first attempt, still to be tested on Azure).

--- train.py
+++ train.py
@@ -10,20 +10,6 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -49,8 +35,8 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
     
-
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
@@ -59,12 +45,26 @@ def main():
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
+    
+    # Fetch the data
+    url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    ds = TabularDatasetFactory.from_delimited_files(url)
 
+    # Spearate features and target
+    x, y = clean_data(ds)
+
+    # Split data into train and test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    # Setup the run
+    run = Run.get_context()
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    # Setup the model
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter,solver='liblinear').fit(x_train, y_train)
 
+    # Log the accuracy
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
